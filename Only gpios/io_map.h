20ab7d20ce3eb277c1fde6e4358f36ad3b67767e
Mightchangeitlater <87609045+Zoubr99@users.noreply.github.com>
2024-09-15 14:17:54 +0100

    only gpios

---
+++ Only gpios/io_map.h
@@ -0,0 +1,25 @@
+#ifndef _IO_MAP_INCLUDED
+#define _IO_MAP_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// system clock rate in MHz; used for timer and uart
+#define SYS_CLK_FREQ 100
+
+// io base adress for uBlaze MCS
+#define BRIDGE_BASE 0xc0000000
+
+// slot module definition
+// format: Slot#_ModuleType_Name
+#define S2_LED       0
+#define S3_SW        1
+
+#ifdef __cplusplus
+} // exetrn "C"
+#endif
+
+
+
+#endif
