48c83813ca59ef6f1330e320e0ad3fabcc0274da
Mightchangeitlater <87609045+Zoubr99@users.noreply.github.com>
2024-09-18 16:07:38 +0200

    full soc

---
+++ Full_SoC/testing_code.cpp
@@ -0,0 +1,96 @@
+
+//#define _DEBUG
+#include "init.h"
+#include "gpio_cores.h"
+
+/*
+// blink once per second for 5 times.
+// provide a sanity check for timer (based on SYS_CLK_FREQ)
+// led_p pointer to led instance
+void timer_check(GpoCore *led_p) {
+   int i;
+
+   for (i = 0; i < 5; i++) {
+      led_p->write(0xffff);
+      sleep_ms(500);
+      led_p->write(0x0000);
+      sleep_ms(500);
+      //debug("timer check - (loop #)/now: ", i, now_ms());
+   }
+}
+
+
+// check individual led
+// led_p pointer to led instance
+// n number of led
+void led_check(GpoCore *led_p, int n) {
+   int i;
+
+   for (i = 0; i < n; i++) {
+      led_p->write(1, i);
+      sleep_ms(200);
+      led_p->write(0, i);
+      sleep_ms(200);
+   }
+}
+
+
+// leds flash according to switch positions.
+// led_p pointer to led instance
+// sw_p pointer to switch instance
+void sw_check(GpoCore *led_p, GpiCore *sw_p) {
+   int i, s;
+
+   s = sw_p->read();
+   for (i = 0; i < 30; i++) {
+      led_p->write(s);
+      sleep_ms(50);
+      led_p->write(0);
+      sleep_ms(50);
+   }
+}
+
+*/
+
+// instantiate switch, led
+GpoCore led(get_slot_addr(BRIDGE_BASE, S2_LED));
+GpiCore sw(get_slot_addr(BRIDGE_BASE, S3_SW));
+GpoCore *led_p;
+GpiCore *sw_p;
+
+int main()
+{
+
+   int n = 16;
+   int s;
+
+   while (1) 
+   {
+      //timer_check(&led);
+      for (int i = 0; i < 5; i++) {
+      led_p->write(0xffff);
+      sleep_ms(500);
+      led_p->write(0x0000);
+      sleep_ms(500);
+      debug("timer check - (loop #)/now: ", i, now_ms());
+      }
+
+      //led_check(&led, 16);
+      for (int i = 0; i < n; i++){
+      led_p->write(1, i);
+      sleep_ms(200);
+      led_p->write(1, i);
+      sleep_ms(200);
+      }
+
+      //sw_check(&led, &sw);
+      s = sw_p->read();
+      for (int i = 0; i < 30; i++) {
+      led_p->write(s);
+      sleep_ms(50);
+      led_p->write(0);
+      sleep_ms(50);
+      }
+   }
+} 
+
--- Only gpios/only gpos/testing_code.cpp
+++ Only gpios/only gpos/testing_code.cpp
@@ -1,32 +1,19 @@
 #include "gpio_cores.h"
 
 
-
-
-// check individual led
-// led_p pointer to led instance
-// n number of led
-void led_check(GpoCore *led_p, int n) {
-   int i;
-
-   for (i = 0; i < n; i++) {
-
-
-      led_p->write(1);
-
-   }
-}
-
 // instantiate switch, led
 GpoCore led(get_slot_addr(BRIDGE_BASE, S2_LED));
-
+GpoCore *led_p;
 
 int main()
 {
-
+   int n = 16;
+   int i;
    while (1) 
    {
-      led_check(&led, 16);
+      for (i = 0; i < n; i++){
+      led_p->write(1);
+      }
    }
 } 
 
